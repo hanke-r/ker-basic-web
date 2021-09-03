87145e54daba6e2e0daa261a3d6ea271e5e77618
hanjaeok17 <hanjaeok17@illuvation.kr>
2021-09-03 16:00:58 +0900

    2021. 09. 03 ker-basic-web - first commit (default settings) - Spring boot - dependency   -. web   -. lombok   -. devtools   -. thymeleaf

---
+++ src/main/java/com/hanker/kerbasicweb/KerBasicWebApplication.java
@@ -0,0 +1,13 @@
+package com.hanker.kerbasicweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KerBasicWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KerBasicWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanker/kerbasicweb/KerBasicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanker.kerbasicweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KerBasicWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
